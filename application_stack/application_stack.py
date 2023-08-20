a73c96588ec3dfa0f82247d8aadab4689957be6a
siobhanmt3 <martinezsiobhan0@gmail.com>
2023-08-20 00:05:04 -0600

    fix: pass mongodb uri as a context

--- application_stack/application_stack.py
+++ application_stack/application_stack.py
@@ -18,6 +18,7 @@ class ApplicationStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
         
         docker_tag = self.node.try_get_context("docker_tag")
+        mongodb_uri = self.node.try_get_context("docker_tag")
 
         vpc = ec2.Vpc(
             self,
@@ -59,9 +60,12 @@ class ApplicationStack(Stack):
             task_image_options=ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
                 image=ecr_image,
                 task_role=ecs_task_role,
+                environment={
+                    "MONGODB_URI": mongodb_uri
+                }
             ),
         )
 
         fargate_cluster.target_group.configure_health_check(
-            path="/docs"
+            path="/healthcheck"
         )
